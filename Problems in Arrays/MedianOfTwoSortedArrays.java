2bcfc5108eba4cbffd45c6578e2bf54160a6256b
SudiptoChakraborty407 <sudipto.chakraborty407@gmail.com>
2025-11-14 06:55:50 +0530

    MedianOfTwoSortedArrays

--- Problems in Arrays/MedianOfTwoSortedArrays.java
+++ Problems in Arrays/MedianOfTwoSortedArrays.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class MedianOfTwoArrays {
+class MedianOfTwoSortedArrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Arr1");
